61cc3c9e73a7e55ec0a90be3617e16b3f6189c77
Gaurav Tak <gaurav.tak@roro.io>
2023-12-10 19:00:30 +0530

    Standard Folder structure created

--- SwiftUI-Viper-Architecture/ContentView.swift
+++ SwiftUI-Viper-Architecture/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("swiftui_icon1").resizable()
+                .frame(width: 60, height: 60)
+            Text("SwiftUI with viper Architecture")
         }
         .padding()
     }
