7e660565438891782f5dcc802c8099d9b44860c7
Gaurav Tak <gaurav.tak@roro.io>
2023-12-10 17:14:54 +0530

    Initial Commit

---
+++ SwiftUI-Viper-Architecture/SwiftUI_Viper_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Viper_ArchitectureApp.swift
+//  SwiftUI-Viper-Architecture
+//
+//  Created by Roro Solutions LLP on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Viper_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
